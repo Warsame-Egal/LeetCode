6c908d8feda551008a4897c8578230c5420f4c7f
warsame egal <warsameegal0@gmail.com>
2022-10-27 21:42:08 -0400

    ValidParentheses

---
+++ ValidParentheses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+        }
+            public static bool IsValid(string s)
+            {
+                Stack<Char> stack = new Stack<Char>();
+
+                foreach (char c in s)
+                {
+                    if (c == '(')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == '{')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == '[')
+                    {
+                        stack.Push(c);
+                    }
+                    else if (c == ')')
+                    {
+                        if (stack.Peek().ToString() == "(") { stack.Pop(); }
+                        else
+                            return false;
+                    }
+                    else if (c == '}')
+                    {
+                        if (stack.Peek().ToString() == "{") { stack.Pop(); }
+                        else
+                            return false;
+                    }
+                    else if (c == ']')
+                    {
+                        if (stack.Peek().ToString() == "[") { stack.Pop(); }
+                        else
+                            return false;
+                    }
+                }
+                return true;
+            }
+        }
+    
+}
